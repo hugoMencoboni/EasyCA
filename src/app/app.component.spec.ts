25889c1c7c34463495f0cb2141d5b109c68477b8
hugo.M <hugo.mencoboni@gmail.com>
2020-08-26 10:39:35 +0200

    Fix: renaming project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CanadaConverter'`, () => {
+  it(`should have as title 'EasyCA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CanadaConverter');
+    expect(app.title).toEqual('EasyCA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CanadaConverter app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('EasyCA app is running!');
   });
 });
